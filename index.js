0b184be87d6b3ac4d4e944cb04d6238dc44cd4cb
EPetrineLynghaug <136695793+EPetrineLynghaug@users.noreply.github.com>
2024-02-04 15:50:09 +0100

    Api update.

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 // import
 
-
-
 // Laget variabler for API og token
-const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IlBldHJpbmUiLCJpYXQiOjE3MDY4NjI2OTh9.WfQZ4-t5gGc1pVkdtr5dokVca2XtK5VXLaxqqV1WK5o';
+const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IlBldHJpbmUiLCJpYXQiOjE3MDcwNTcxNDR9.Agi6gGucuhgfZd5q_4d0AuUo-lGsqNA5xQCJIBlmSz4';
 const baseUrl = 'https://api.noroff.dev/api/v1/rainy-days';
 const prodContainer = document.getElementById('productsContainer');
 const loading = document.querySelector('.loading');
@@ -20,14 +18,16 @@ async function getProducts() {
                 'Authorization': `Bearer ${token}`
             }
         });
-        console.log(response);
 
         if (response.ok) {
             // Data parsed with .json()
             const products = await response.json();
-            console.log(products);
+            console.log(products)
+
+            // Create product cards
             displayProducts(products);
-            // loading.style.display = 'none';
+
+            // Hide loading
             loading.classList.add('hidden');
         } else {
             displayError('Kunne ikke hente produkter');
@@ -38,12 +38,14 @@ async function getProducts() {
     } catch (error) {
         // Error hvis request feilet
 
-        displayError('Kunne ikke hente produkter');
+        displayError('Noe gikk galt...');
 
         // TODO: Håndter error/vis error til bruker
 
     }
 }
+
+getProducts();
 
 function displayError(message) {
     console.log(message);
@@ -51,73 +53,38 @@ function displayError(message) {
 
 function displayProducts(products) {
     products.map(product => {
-        console.log(product);
-        // Lager denne: <section class="card">
-        let card = createElement('section', ['card']);
+        let card = document.createElement('section');
+        card.classList.add('card');
 
-        // Lager denne: <div class="card-image">
-        let cardImage = createElement('div', ['card-image']);
+        card.innerHTML = `<div class="card-image">
+    <a href="./ulriken.html">
+    <img
+        src="${product.image}"
+        alt="100% wool jacket, sturdy zipper, good pockets on each side."
+    />
+    </a>
+</div>
 
-        let cardContent = createElement('div', ['card-content']);
-
-        let h2 = createElement('h2', ['product-card-title'], product.title);
-        cardContent.append(h2);
-
-        card.append(cardImage);
-        card.append(cardContent);
+<div class="card-content">
+    <h2>${product.title}<small> ${product.price},- kr</small></h2>
+    <p class="card-description">${product.description}</p>
+    <div class="card-actions">
+    <div class="card-quantity">
+        <label for="sizes1">Størrelse</label>
+        <select name="size" id="sizes1">
+        <option value="xs">XS</option>
+        <option value="small">Small</option>
+        <option value="medium">Medium</option>
+        <option value="large">Large</option>
+        <option value="x-large">X-Large</option>
+        </select>
+    </div>
+    <a href="./ulriken.html" class="card-button button-secondary">Vis produkt</a>
+    <a href="./ulriken.html" class="product-button product-primary">
+        <i class="fa-solid fa-cart-plus"></i>Kjøp</a>
+    </div>
+</div>`;
 
         prodContainer.append(card);
     });
 }
-
-function createElement(tag, classes, text) {
-    let newElement = document.createElement(tag);
-    classes.map(classname => newElement.classList.add(classname));
-
-    if (text) {
-        newElement.innerText = text;
-    }
-
-    return newElement;
-}
-
-getProducts();
-
-// <section class="card">
-//     <div class="card-image">
-//         <a href="./ulriken.html">
-//         <img
-//             src="./media/RainyDays_Jacket7_1_11zon.jpg"
-//             alt="100% wool jacket, sturdy zipper, good pockets on each side."
-//         />
-//         </a>
-//     </div>
-// 
-//     <div class="card-content">
-//         <h2>Ulriken 100% ulljakke <small>-kr 1699.-</small></h2>
-//         <p class="card-description">
-//         Møt "Ulriken 100% ull" - vår lokale stolthet. Håndlaget i Bergen,
-//         denne jakken er både tradisjonell og førsteklasses. Laget av
-//         førsteklasses ull, gir den varme og stil til eventyrene dine. Med
-//         vår dedikasjon til lokal produksjon får du en jakke som beskytter
-//         mot elementene, uansett hvor du går i Bergen. Velg "Ulriken 100%
-//         ull" for varme, stil og tilknytning til vår kulturarv. En jakke som
-//         er like egnet for fjellturer som for bylivet.
-//         </p>
-//         <div class="card-actions">
-//         <div class="card-quantity">
-//             <label for="sizes1">Størrelse</label>
-//             <select name="size" id="sizes1">
-//             <option value="xs">XS</option>
-//             <option value="small">Small</option>
-//             <option value="medium">Medium</option>
-//             <option value="large">Large</option>
-//             <option value="x-large">X-Large</option>
-//             </select>
-//         </div>
-//         <a href="./ulriken.html" class="card-button button-secondary">Vis produkt</a>
-//         <a href="./ulriken.html" class="product-button product-primary">
-//             <i class="fa-solid fa-cart-plus"></i>Kjøp</a>
-//         </div>
-//     </div>
-// </section>
